7c0a0c6635fe63cc06469fee6025db37ce6f274f
andrii33 <andynew3@gmail.com>
2015-01-21 00:17:41 +0200

    disable error

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-// error_reporting(E_ALL);
-error_reporting(-1);
-ini_set('display_errors', 'On');
+ error_reporting(E_ALL);
+//error_reporting(-1);
+//ini_set('display_errors', 'On');
  $site_path = realpath(dirname(__FILE__));
  define ('__SITE_PATH', $site_path);
 
